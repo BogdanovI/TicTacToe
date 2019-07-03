d37aaa97d1c7d74e1106c9b2233ad9c786e06127
BogdanovI <likoz@bk.ru>
2019-07-03 19:14:23 +0300

    Main menu
    The menu implementation has been reworked.

--- main.cpp
+++ main.cpp
@@ -10,8 +10,14 @@ HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 class mainMenu
 {
+private:
+    string playGame {"Play game<"};
+    string controlButtons {"Control buttons"};
+    string exitGame {"Exit"};
+    int cursorPosition = 1;
+    char cursorMenu = '<';
 public:
-    mainMenu()
+    void drawLogo()
     {
             SetConsoleTextAttribute(hConsole, 2);
             cout << setw(53) << "TIC-";
@@ -21,102 +27,85 @@ public:
             cout << "TOE";
             SetConsoleTextAttribute(hConsole, 7);
     }
-};
-
-class firstMenu : public mainMenu
-{
-public:
-    string playGame {"Play game"};
-    string controlButtons {"Control buttons"};
-    string exitGame {"Exit"};
-    int cursorPosition;
-    char cursorMenu = '<';
-
-    int showFirstMenu()
+    void drawFirstMenu()
     {
-        int pressedButton;
         cout << setw(57) << endl << playGame;
         cout << setw(63) << endl << controlButtons;
         cout << setw(52) << endl << exitGame;
-        pressedButton = getch();
-        system("cls");
-        mainMenu();
-        return pressedButton;
     }
     void moveCursor()
     {
-        int cursorPosition = 1;
-        playGame.push_back(cursorMenu);
-            while (showFirstMenu() != 13)
+        int pressedButton = getch();
+        while (pressedButton)
+        {
+            if(pressedButton == 115 || pressedButton == 119)
             {
-                if(showFirstMenu() == 115 || showFirstMenu() == 119)
+                if(cursorPosition == 1)
                 {
-                    if(cursorPosition == 1)
+                    if(pressedButton == 115)
                     {
-                        if(showFirstMenu() == 115)
-                        {
-                            playGame = playGame.substr(0, playGame.size() - 1);
-                            controlButtons.push_back(cursorMenu);
-                            cursorPosition = 2;
-                        }
+                        playGame = playGame.substr(0, playGame.size() - 1);
+                        controlButtons.push_back(cursorMenu);
+                        cursorPosition = 2;
                     }
-                    if(cursorPosition == 1)
+                    if(pressedButton == 119)
                     {
-                        if(showFirstMenu() == 119)
-                        {
-                            playGame = playGame.substr(0, playGame.size() - 1);
-                            exitGame.push_back(cursorMenu);
-                            cursorPosition = 3;
-                        }
-                    }
-                    if(cursorPosition == 2)
-                    {
-                        if(showFirstMenu() == 115)
-                        {
-                            controlButtons = controlButtons.substr(0, controlButtons.size() - 1);
-                            exitGame.push_back(cursorMenu);
-                            cursorPosition = 3;
-                        }
-                    }
-                    if(cursorPosition == 2)
-                    {
-                        if(showFirstMenu() == 119)
-                        {
-                            controlButtons = controlButtons.substr(0, controlButtons.size() - 1);
-                            playGame.push_back(cursorMenu);
-                            cursorPosition = 1;
-                        }
-                    }
-                    if(cursorPosition == 3)
-                    {
-                        if(showFirstMenu() == 115)
-                        {
-                            exitGame = exitGame.substr(0, exitGame.size() - 1);
-                            playGame.push_back(cursorMenu);
-                            cursorPosition = 1;
-                        }
-                    }
-                    if(cursorPosition == 3)
-                    {
-                        if(showFirstMenu() == 119)
-                        {
-                            exitGame = exitGame.substr(0, exitGame.size() - 1);
-                            controlButtons.push_back(cursorMenu);
-                            cursorPosition = 2;
-                        }
+                        playGame = playGame.substr(0, playGame.size() - 1);
+                        exitGame.push_back(cursorMenu);
+                        cursorPosition = 3;
                     }
                 }
-                showFirstMenu();
-            };
+                else if(cursorPosition == 2)
+                {
+                    if(pressedButton == 115)
+                    {
+                        controlButtons = controlButtons.substr(0, controlButtons.size() - 1);
+                        exitGame.push_back(cursorMenu);
+                        cursorPosition = 3;
+                    }
+                    if(pressedButton == 119)
+                    {
+                        controlButtons = controlButtons.substr(0, controlButtons.size() - 1);
+                        playGame.push_back(cursorMenu);
+                        cursorPosition = 1;
+                    }
+                }
+                else if(cursorPosition == 3)
+                {
+                    if(pressedButton == 115)
+                    {
+                        exitGame = exitGame.substr(0, exitGame.size() - 1);
+                        playGame.push_back(cursorMenu);
+                        cursorPosition = 1;
+                    }
+                    if(pressedButton == 119)
+                    {
+                        exitGame = exitGame.substr(0, exitGame.size() - 1);
+                        controlButtons.push_back(cursorMenu);
+                        cursorPosition = 2;
+                    }
+                }
+                system("cls");
+                drawLogo();
+                drawFirstMenu();
+                moveCursor();
+            }
+            else if (pressedButton == 13)
+            {
+                exit(0);
+            }
+        }
     }
+
 };
-
-
 
 int main()
 {
-    firstMenu newGame;
+    mainMenu newGame;
+    newGame.drawLogo();
+    newGame.drawFirstMenu();
     newGame.moveCursor();
+
 
     return 0;
 }
